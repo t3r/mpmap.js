d9d41fbc43d3dba07c740f2bfd10fd106119583f
Torsten Dreyer <torsten@t3r.de>
2021-09-14 12:52:17 +0200

    new vfrmap cycle

--- static/mpmap.js
+++ static/mpmap.js
@@ -121,7 +121,7 @@ $(function() {
                 transparent: true
             }),
 
-    "VFRMap.com Sectionals (US)" : new L.TileLayer('http://vfrmap.com/20190912/tiles/vfrc/{z}/{y}/{x}.jpg', {
+    "VFRMap.com Sectionals (US)" : new L.TileLayer('http://vfrmap.com/20210909/tiles/vfrc/{z}/{y}/{x}.jpg', {
                 maxZoom : 12,
                 minZoom : 3,
                 attribution : '&copy; <a target="_blank" href="http://vfrmap.com">VFRMap.com</a>',
@@ -130,7 +130,7 @@ $(function() {
                 bounds : L.latLngBounds(L.latLng(16.0, -179.0), L.latLng(72.0, -60.0)),
             }),
 
-    "VFRMap.com - Low IFR (US)" : new L.TileLayer('http://vfrmap.com/20190912/tiles/ifrlc/{z}/{y}/{x}.jpg', {
+    "VFRMap.com - Low IFR (US)" : new L.TileLayer('http://vfrmap.com/20210909/tiles/ifrlc/{z}/{y}/{x}.jpg', {
                 maxZoom : 12,
                 minZoom : 5,
                 attribution : '&copy; <a target="_blank" href="http://vfrmap.com">VFRMap.com</a>',
